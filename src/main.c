98d0c7a0a001c5595b6d4b7e2f6a47971890dd69
alec-heif <alec.heif@gmail.com>
2017-06-12 21:13:06 -0400

    Add read to prevent CAN error, add disregard to prevent overflow of timer

--- src/main.c
+++ src/main.c
@@ -18,7 +18,7 @@ const uint32_t OscRateIn = 12000000;
 #define HEARTBEAT_PERIOD_MS 1000
 #define WHEEL_SPEED_PERIOD_MS 20
 
-#define CLICK_TIMEOUT_MS 1000
+#define WHEEL_SPEED_TIMEOUT_MS 1000
 
 // 48 MHz (for 32 bit interrupts) = 48M cycles per second
 #define CYCLES_PER_MICROSECOND 48
@@ -44,12 +44,19 @@ uint32_t last_wheel_speed_ms = 0;
 volatile uint32_t last_wheel_1_click = 0;
 volatile uint32_t last_wheel_2_click = 0;
 
+// We want to ensure the first click after a timeout is disregarded.
+// This prevents potential overflow of the timer causing a falsely-low
+// gap between clicks, which would give a falsely-high RPM.
+volatile bool wheel_1_disregard = false;
+volatile bool wheel_2_disregard = false;
+
 static bool resettingPeripheral = false;
 
+void can_read(void);
 void handle_can_error(Can_ErrorID_T error);
 Can_ErrorID_T write_can_heartbeat(void);
 Can_ErrorID_T write_can_wheel_speed(void);
-uint32_t click_time_to_mRPM(uint32_t click_time, uint32_t last_click);
+uint32_t click_time_to_mRPM(uint32_t click_time);
 
 /*****************************************************************************/
 
@@ -66,9 +73,12 @@ void SysTick_Handler(void) {
 void TIMER32_0_IRQHandler(void) {
   Chip_TIMER_Reset(LPC_TIMER32_0);            /* Reset the timer immediately */
   Chip_TIMER_ClearCapture(LPC_TIMER32_0, 0);      /* Clear the capture */
-  wheel_1_clock_cycles_between_ticks = Chip_TIMER_ReadCapture(LPC_TIMER32_0, 0);
+  if (wheel_1_disregard) {
+    wheel_1_clock_cycles_between_ticks = 0;
+  } else {
+    wheel_1_clock_cycles_between_ticks = Chip_TIMER_ReadCapture(LPC_TIMER32_0, 0);
+  }
   last_wheel_1_click = msTicks;
-  Serial_Println("Wheel 1");
 }
 
 // Interrupt handler for timer 1 capture pin. This function get called automatically on
@@ -76,9 +86,12 @@ void TIMER32_0_IRQHandler(void) {
 void TIMER32_1_IRQHandler(void) {
   Chip_TIMER_Reset(LPC_TIMER32_1);            /* Reset the timer immediately */
   Chip_TIMER_ClearCapture(LPC_TIMER32_1, 0);      /* Clear the capture */
-  wheel_2_clock_cycles_between_ticks = Chip_TIMER_ReadCapture(LPC_TIMER32_1, 0);
+  if (wheel_2_disregard) {
+    wheel_2_clock_cycles_between_ticks = 0;
+  } else {
+    wheel_2_clock_cycles_between_ticks = Chip_TIMER_ReadCapture(LPC_TIMER32_1, 0);
+  }
   last_wheel_2_click = msTicks;
-  Serial_Println("Wheel 2");
 }
 
 void Set_Interrupt_Priorities(void) {
@@ -108,6 +121,7 @@ int main(void) {
   Serial_Println("Started up");
 
   while (1) {
+    can_read();
     if (last_wheel_speed_ms + WHEEL_SPEED_PERIOD_MS < msTicks) {
       last_wheel_speed_ms = msTicks;
       handle_can_error(write_can_wheel_speed());
@@ -119,10 +133,21 @@ int main(void) {
   }
 }
 
+void can_read(void) {
+  Can_MsgID_T msgID = Can_MsgType();
+  switch(msgID) {
+    case Can_Error_Msg:
+      // Clear error if there is one for some reason
+      Can_Error_Read();
+      break;
+    case Can_No_Msg:
+    default:
+      break;
+  }
+}
+
 void handle_can_error(Can_ErrorID_T error) {
   if (error != Can_Error_NONE && error != Can_Error_NO_RX) {
-    /* Serial_Print("can_write_err: "); */
-    /* Serial_PrintlnNumber(error, 16); */
     if (!resettingPeripheral) {
       resettingPeripheral = true;
       // TODO add this to CAN library
@@ -135,13 +160,38 @@ void handle_can_error(Can_ErrorID_T error) {
 }
 
 Can_ErrorID_T write_can_wheel_speed(void) {
-    const uint32_t left_click_time = wheel_1_clock_cycles_between_ticks;
-    const uint32_t right_click_time = wheel_2_clock_cycles_between_ticks;
 
     Can_RearCanNode_WheelSpeed_T msg;
 
-    msg.rear_left_wheel_speed_mRPM = click_time_to_mRPM(left_click_time, last_wheel_1_click);
-    msg.rear_right_wheel_speed_mRPM = click_time_to_mRPM(right_click_time, last_wheel_2_click);
+    // Capture values
+    const uint32_t wheel_1_click_time = wheel_1_clock_cycles_between_ticks;
+    const uint32_t wheel_1_last_updated = last_wheel_1_click;
+    const uint32_t wheel_2_click_time = wheel_2_clock_cycles_between_ticks;
+    const uint32_t wheel_2_last_updated = last_wheel_2_click;
+
+    const bool wheel_1_timeout =
+      wheel_1_last_updated + WHEEL_SPEED_TIMEOUT_MS < msTicks;
+    const bool wheel_2_timeout =
+      wheel_2_last_updated + WHEEL_SPEED_TIMEOUT_MS < msTicks;
+
+    if (wheel_1_timeout || wheel_1_click_time == 0) {
+      msg.rear_right_wheel_speed_mRPM = 0;
+    } else {
+      msg.rear_right_wheel_speed_mRPM = click_time_to_mRPM(wheel_1_click_time);
+    }
+    wheel_1_disregard = wheel_1_timeout;
+
+    if (wheel_2_timeout || wheel_2_click_time == 0) {
+      msg.rear_left_wheel_speed_mRPM = 0;
+    } else {
+      msg.rear_left_wheel_speed_mRPM = click_time_to_mRPM(wheel_2_click_time);
+    }
+    wheel_2_disregard = wheel_2_timeout;
+
+    Serial_Print("Left: ");
+    Serial_PrintNumber(msg.rear_left_wheel_speed_mRPM, 10);
+    Serial_Print(", Right: ");
+    Serial_PrintlnNumber(msg.rear_right_wheel_speed_mRPM, 10);
 
     return Can_RearCanNode_WheelSpeed_Write(&msg);
 }
@@ -152,11 +202,7 @@ Can_ErrorID_T write_can_heartbeat(void) {
     return Can_RearCanNode_Heartbeat_Write(&msg);
 }
 
-uint32_t click_time_to_mRPM(uint32_t cycles_per_click, uint32_t last_click) {
-  if (cycles_per_click == 0 || last_click + CLICK_TIMEOUT_MS < msTicks) {
-    return 0;
-  }
-
+uint32_t click_time_to_mRPM(uint32_t cycles_per_click) {
   const uint32_t us_per_click = cycles_per_click / CYCLES_PER_MICROSECOND;
   const uint32_t us_per_rev = us_per_click * CLICKS_PER_REV;
 
